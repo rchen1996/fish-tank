e9341f78ee0b0135571229ac3640fa605ce3ba63
Rebecca Chen <rebecca061996@gmail.com>
2021-01-18 01:22:17 +0000

    added seaweed

--- public/scripts/driver.js
+++ public/scripts/driver.js
@@ -7,6 +7,36 @@ $(() => {
     position: new Vector(0, 0)
   });
 
+  const seaweed = new Seaweed({
+    tank: window.fishtank,
+    position: new Vector(-150, 0)
+  })
+
+  const seaweed2 = new Seaweed({
+    tank: window.fishtank,
+    position: new Vector(-300, 0)
+  })
+
+  const seaweed3 = new Seaweed({
+    tank: window.fishtank,
+    position: new Vector(-450, 0)
+  })
+
+  const seaweed4 = new Seaweed({
+    tank: window.fishtank,
+    position: new Vector(150, 0)
+  })
+
+  const seaweed5 = new Seaweed({
+    tank: window.fishtank,
+    position: new Vector(300, 0)
+  })
+
+  const seaweed6 = new Seaweed({
+    tank: window.fishtank,
+    position: new Vector(450, 0)
+  })
+
 });
 
 
