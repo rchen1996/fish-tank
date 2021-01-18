9726836aa1d0c718323f129c6a0ebbb44f36bb71
Rebecca Chen <rebecca061996@gmail.com>
2021-01-18 01:38:27 +0000

    added an effect to the starter for when it's clicked

--- public/scripts/entities/starter.js
+++ public/scripts/entities/starter.js
@@ -11,13 +11,28 @@ class Starter extends Denizen {
   }
 
   onClick(event) {
-    var xVel = randRangeInt(-300, 300);
-    var yVel = 400 - Math.abs(xVel);
-    var s = new Seed({
-      tank: this.tank,
-      position: this.position,
-      velocity: new Vector(xVel, yVel),
-      type: this.tank.getRandomSpecies(),
-    });
+    setTimeout(() => {
+      this.height *= 1.5
+    }, 50)
+
+    setTimeout(() => {
+      this.height *= 1.5
+      var xVel = randRangeInt(-300, 300);
+      var yVel = 400 - Math.abs(xVel);
+      var s = new Seed({
+        tank: this.tank,
+        position: this.position,
+        velocity: new Vector(xVel, yVel),
+        type: this.tank.getRandomSpecies(),
+      });
+    }, 100)
+
+    setTimeout(() => {
+      this.height /= 1.5
+    }, 150)
+
+    setTimeout(() => {
+      this.height /= 1.5
+    }, 200)
   }
 }
