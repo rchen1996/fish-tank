4e12a43851b9810c4a2240b18ff26879486b4ee3
Rebecca Chen <rebecca061996@gmail.com>
2021-01-18 00:04:12 +0000

    updated image

--- public/scripts/entities/switch_fish.js
+++ public/scripts/entities/switch_fish.js
@@ -1,5 +1,10 @@
 class SwitchFish extends Fish {
 
+  constructor(options) {
+    super(options);
+    this.imageUri = '/images/switch-fish.png';
+  }
+
   onClick(event) {
     this.makeNewVelocity(50);
   }
